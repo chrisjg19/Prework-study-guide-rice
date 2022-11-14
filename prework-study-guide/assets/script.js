efd475b70226b274eb9b635812ce9ecccc2f97fc
Chritopher Gonzalez <christopher.gonzalez98@gmail.com>
2022-11-14 17:00:29 -0600

    script

---
+++ prework-study-guide/assets/script.js
@@ -0,0 +1,18 @@
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for (var x = 0; x < topics.length; x++){
+    console.log(topics[x]);
+}
+/*
+if (topic === 'HTML') {
+    console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+    console.log("Let's Study CSS!")
+} else if (topic === 'Git') {
+    console.log("Let's Study Git!");
+} else if (topic === 'JavaScript') {
+    console.log("Let's Study JavaScript!");
+} else {
+    console.log('Please try Again!');
+}
+*/
